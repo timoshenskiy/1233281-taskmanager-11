f0caad4d15bc3749cf8f3b696f0d7046abf59986
Alexandr Timoshenskiy <timoshenskiy.ad@gmail.com>
2020-04-15 15:59:40 +0300

    Установил количество карточек на 20

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import {generateFilters} from "./mock/filter.js";
 import {render, RenderPosition} from "./utils.js";
 
 
-const TASK_COUNT = 0;
+const TASK_COUNT = 20;
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
