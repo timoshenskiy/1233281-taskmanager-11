8a84075d1e99a1312b2dd4a1bd08ae013bf5a94e
Alexandr Timoshenskiy <timoshenskiy.ad@gmail.com>
2020-04-18 15:32:37 +0300

    Учебный проект: разделяй и властвуй (Часть 1)

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,4 +1,5 @@
-import {createElement} from "../utils.js";
+import AbstractComponent from "./abstract-component.js";
+
 const createSortTemplate = () => {
   return (`<div class="board__filter-list">
       <a href="#" class="board__filter">SORT BY DEFAULT</a>
@@ -8,25 +9,9 @@ const createSortTemplate = () => {
   );
 };
 
-export default class Sort {
-  constructor() {
-    this._element = null;
-  }
-
+export default class Sort extends AbstractComponent {
   getTemplate() {
     return createSortTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
 
